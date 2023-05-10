9792df76add73ca45e10b538e3dcb6e3a34b479f
Yudhasu <133074181+Yudhasu@users.noreply.github.com>
2023-05-10 09:45:35 +0700

    upload ke github

---
+++ pengurangan.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pengurangan</title>
+</head>
+<body>
+	<?php
+		$angka_pertama = 15;
+		$angka_kedua = 45;
+		$angka_ketiga = 100;
+		$jumlah = $angka_pertama + $angka_kedua - $angka_ketiga ;
+			
+		echo "Jawaban dari " . $angka_pertama ." + " . $angka_kedua ." - " . $angka_ketiga . " adalah " . $jumlah. ".";
+
+	?>
+</body>
+</html>
